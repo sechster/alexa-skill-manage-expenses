c0e31292bc9dedbefcfef593d765d5c818423639
sechster <30355592+sechster@users.noreply.github.com>
2017-12-10 10:53:43 +0100

    Init

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+exports.handler = (event, context, callback) => {
+    // TODO implement
+    
+    // TODO: 
+    // 1. process arguments
+    // 2. connect to gdrive
+    // 3. find document
+    // 4. find spreadsheet for the month
+    // 5. find last row
+    // 6. input data
+    callback();
+};
