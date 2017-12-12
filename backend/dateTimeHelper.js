47b4c2fe34b294ca524837c5c1fba1ae47d8b625
sechster <30355592+sechster@users.noreply.github.com>
2017-12-12 21:27:48 +0100

    Clean up

--- backend/dateTimeHelper.js
+++ backend/dateTimeHelper.js
@@ -7,12 +7,6 @@ module.exports = (function dateTimeHelperModule()
         return moment().year();
     }
     
-    function getCurrentMonth()
-    {
-        let month = moment().month() + 1;
-        return ("0" + month).substr(-2);
-    }
-    
     function getCurrentDay()
     {
         return moment().date();
@@ -20,7 +14,6 @@ module.exports = (function dateTimeHelperModule()
 
     let publicAPI = {
         getCurrentYear: getCurrentYear,
-        getCurrentMonth: getCurrentMonth,
         getCurrentDay: getCurrentDay
     };
 
