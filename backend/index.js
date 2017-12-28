9af55b27685de4f5d70985f1b4891afe58af9a07
sechster <30355592+sechster@users.noreply.github.com>
2017-12-28 16:28:42 +0100

    Problems with the pronunciation

--- backend/index.js
+++ backend/index.js
@@ -3,26 +3,40 @@ const moment = require("moment");
 
 const APP_ID = 'amzn1.ask.skill.554f7f97-69da-43f3-a5b5-095a3e3bd3d9';
 const categories = "regular, health, bills, car, entertainment, presents, clothes, other, yearly";
-const HELP_MESSAGE = `Just say ask expense manager to add new expense with category (something) description (something) and amount (something). Categories are: ${categories}.`;
+const HELP_MESSAGE = `Just say: ask financial to insert new cost with category (something) description (something) and amount (something) point (somehing). For categories ask: what are the categories.`;
 const STOP_MESSAGE = 'Stopping';
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.appId = APP_ID;
+    console.log("Expenses - executing");
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
 const handlers = {
     'LaunchRequest': function () {
+        console.log("Expenses - LaunchRequest");
         this.emit('AddExpenseIntent');
     },
+    'Unhandled': function() {
+        console.log("Expenses - Unhandled");
+        this.response.speak("I do not know what you are saying double o seven");
+        this.emit(':ask', 'There was an error. Check the logs.');
+    },
     'AddExpenseIntent': function () {
+        console.log("Expenses - AddExpenseIntent");
+
         if (!isSlotValid(this.event.request, "category")
             || !isSlotValid(this.event.request, "description")
-            || !isSlotValid(this.event.request, "amount")) {
+            || !isSlotValid(this.event.request, "amountInteger")
+            || !isSlotValid(this.event.request, "amountFraction")) {
+                console.log("Expenses - AddExpenseIntent - delegate slot collection");
                 delegateSlotCollection(this.event, this.emit);
         } else {
+
+            console.log("Expenses - AddExpenseIntent - adding");
+
             const ExpenseService = require("./expenseService");
 
             let year = moment().year();
@@ -30,7 +44,10 @@ const handlers = {
             let day = moment().date();
             let category = this.event.request.intent.slots.category.value;
             let description = this.event.request.intent.slots.description.value;
-            let amount = this.event.request.intent.slots.amount.value;
+            let amountInteger = this.event.request.intent.slots.amountInteger.value;
+            let amountFraction = this.event.request.intent.slots.amountFraction.value;
+
+            let amount = parseFloat(amountInteger + '.' + amountFraction);
 
             let self = this;
 
@@ -80,6 +97,9 @@ function delegateSlotCollection(event, emit){
 }
 
 function isSlotValid(request, slotName){
-    var slot = request.intent.slots[slotName];
-    return (slot && slot.value);
+    return (
+        request.intent 
+            && request.intent.slots 
+            && request.intent.slots[slotName] 
+            && request.intent.slots[slotName].value);
 }
